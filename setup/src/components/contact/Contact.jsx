ff95ba05b7a69fa2c98a70b6a1fa6c00b60ddaa3
Visanth1135 <k.visanth1996@gmail.com>
2022-05-26 22:59:37 +0400

    Portfolio template completed

--- setup/src/components/contact/Contact.jsx
+++ setup/src/components/contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+
 
 const Contact = () => {
   return (
-    <section id = 'contact'>Contact</section>
+    <section id = 'contact'>
+    <h5>Get In Touch</h5>
+<h2>Contact Me</h2>
+<div className="container contact container">
+  <div className="contact_options">
+     <article className="contact_option">
+        <MdOutlineEmail className='contact_option-icon'/>
+        <h4>Email</h4>
+        <h5>dummyegator@gmail.com</h5>
+        <a href="mailto:dummyegator@gmail.com"> Sendamessage </a>
+     </article>
+     <article className="contact_option">
+        <MdOutlineEmail className='contact_option-icon'/>
+        <h4>Messenger</h4>
+        <h5>egatortutorials</h5>
+        <a href="https://m.me/ernest.achiever"> Sendamessage </a>
+     </article>
+     <article className="contact_option">
+        <BsWhatsapp className='contact_option-icon'/>
+        <h4>Whatsapp</h4>
+        <h5>123456789</h5>
+        <a href="https://api.whatsapp.com/send?phone+123456789"> Sendamessage </a>
+     </article>
+     </div>
+     <form action="">
+  <input type="text" name='name' placeholder='Your Full Name' required/>
+  <input type="email" name='email' placeholder='Your Email' required/>
+  <textarea name="message" rows='7' placeholder='Your Message' required></textarea>
+  <button type='submit' className='btn btn-primary'>Send Message</button>
+</form>
+     </div>
+    </section>
   )
 }
 
--- setup/src/components/testimonials/Testimonials.jsx
+++ setup/src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 import './testimonial.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import{Pagination, Navigation}from'swiper';
+import{Swiper,SwiperSlide}from'swiper/react';
+// Import Swiper styles
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+
+const data=[
+ {
+    avatar:AVTR1,
+    name:'Tina Snow',
+    review:'jkbhkk',
+ },
+ {
+    avatar:AVTR2,
+    name:'Shatta Wale',
+    review:'jhvhj',
+},
+{
+    avatar:AVTR3,
+    name:"Kwame Despite",
+    review:'bnkk',
+},
+{
+  avatar:AVTR4,
+  name:"Kwame Despite",
+  review:'bnkk',
+},
+]
+
 const Testimonials = () => {
   return (
-    <section id = 'testimonials'>Testimonials</section>
+    <section id = 'testimonials'>
+    <h5>Review from clients</h5>
+<h2>Testimonials</h2>
+<Swiper className="container testimonials_container"
+
+spaceBetween={40}
+slidesPerView={1}
+navigation
+pagination={{clickable:true}}>
+  {
+     data.map(({avatar,name,review},index)=>{
+        return(
+           <SwiperSlide key={index}className="testimonial">
+           <div className="client_avatar">
+              <img src={avatar}/>
+           </div>
+           <h5 className='client_name'>{name}</h5>
+           <small className='client_review'>{review}</small>
+        </SwiperSlide>
+        )
+  })}
+</Swiper>
+    </section>
   )
 }
 
